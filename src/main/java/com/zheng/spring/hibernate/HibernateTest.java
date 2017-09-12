64b42bca23a98ad16093923efb9d4a6f23f6080e
Zheng Gong <zgong@vistaprint.com>
2017-09-12 00:19:15 -0400

    1.5 Saving Objects using Hibernate APIs

---
+++ src/main/java/com/zheng/spring/hibernate/HibernateTest.java
@@ -0,0 +1,22 @@
+package com.zheng.spring.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.zheng.spring.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+		user.setUserId(1);
+		user.setUserName("First User");
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+}
